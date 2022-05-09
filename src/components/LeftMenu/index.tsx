c838b24d00a9d0468ec03c55db10da799beae2ef
Ignacio <nachodiaz99@gmail.com>
2022-05-08 21:48:08 -0400

    drawer done

--- src/components/LeftMenu/index.tsx
+++ src/components/LeftMenu/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
-import { Drawer } from '@mui/material'
+import { Drawer, Box, List, ListItem, ListItemIcon, ListItemText, Divider } from '@mui/material'
+import InboxIcon from '@mui/icons-material/Inbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 function LeftMenu(props: any) {
-  return (
-    <Drawer open={props.open} />
-  )
+
+    return (
+        <Drawer open={props.open}>
+            <Box sx={{ width: 250 }} role="presentation"
+        //   onClick={toggleDrawer(anchor, false)}
+        //   onKeyDown={toggleDrawer(anchor, false)}
+            >
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+                </List>
+            </Box>
+        </Drawer>
+    )
 }
 
 export default LeftMenu;
